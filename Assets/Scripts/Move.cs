522cfe47e0bf6bb9ad54e2913dfc4cedfdb5f9b8
ZeFurtado <lukhasfurtado9@gmail.com>
2023-11-13 00:51:38 -0300

    Primeiro commit \o/

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Bem-Vindo ao Scooby-doo! Mayan Mayhem");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
